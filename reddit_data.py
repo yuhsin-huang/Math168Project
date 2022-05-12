2ffbc0197d71737120953768cefae32b901393a2
christinegu27 <christinegu27@ucla.edu>
2022-05-11 17:29:19 -0700

    fixing the last commit

--- reddit_data.py
+++ reddit_data.py
@@ -1,25 +1,49 @@
 import praw
-import csv
+from psaw import PushshiftAPI
+import pandas as pd
+import numpy as np
+
+def find_submission(ids):
+    try:
+        submission = reddit.submission(id = ids['Submission'])
+        ids['Submission_Author'] = submission.author
+        ids['Submission_Title'] = submission.title
+    except prawcore.exceptions.NotFound:
+        ids['Submission_Author'] = 'idk'
+        ids['Submission_Title'] = 'removed'
+    return ids
 
 reddit = praw.Reddit(
-    client_id = '',
-    client_secret="",
-    user_agent="",
-    username="",
-    password=""
+
 )
 
 
-subreddit = reddit.subreddit("covidvaccine").top("year") #gets top submissions in r/covidvaccine in the past year
+api = PushshiftAPI()
 
-with open("reddit_demo.csv", 'a', encoding="utf-8") as file:
-    headers = ['ID', 'Date_utc', 'Author', 'Body','Post_ID', 'Post_Author']
-    writer  = csv.DictWriter(file, fieldnames = headers, extrasaction='ignore')
-    writer.writeheader()
-    for post in subreddit:
-        post.comments.replace_more()
-        for comment in post.comments:
-            data = {'ID':comment.id, 'Date_utc':comment.created_utc, 'Author':comment.author, 'Body': comment.body,
-            'Post_ID':post, 'Post_Author':post.author}
-            writer.writerow(data)
+subreddits = ['trueantivaccination','vaccines','covidvaccine','covidvaccinated','antivaxxers',
+'conspiracy', 'conspiracytheories','conspiracy_commons', 'covid19','coronavirus', 'conservative', 'news'] 
+
+#worldnews
+
+subreddit_data = []
+
+
+for subreddit in subreddits:
+    url = f"https://raw.githubusercontent.com/yuhsin-huang/Math168Project/main/{subreddit}.csv"
+    data = pd.read_csv(url)
+    data = data.dropna(subset = ['Author'])
+
+    submissions = pd.DataFrame(data = {'Submission':data['Submission'].unique()})
+    submissions = submissions.apply(find_submission, axis = 1)
+
+    data_joined = data.merge(submissions, how = 'left', on = 'Submission')
+
+    data_joined = data_joined.dropna(subset = ['Author'])
+    data_joined = data_joined.dropna(subset = ['Parent_Author', 'Submission_Author'], how = 'all')
+    data_joined = data_joined[total_data["Author"] != "AutoModerator"]
+
+    subreddit_data.append(data_joined)
+
+reddit_data = pd.concat(subreddit_data, ignore_index = True)
+reddit_data.to_csv("C:\\Users\\chris\\Documents\\Math168Project\\reddit_data.csv", index = False)
 
